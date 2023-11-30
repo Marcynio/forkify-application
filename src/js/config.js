06e94ae61ea53c52d984eaf8171d643008125220
Marcin Cydejko <marcin.cydejko98@gmail.com>
2023-11-30 15:55:03 +0100

    Forkify application

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '61bc7a9a-3cff-4aa1-a789-b0e8746b3ad4';
+export const MODAL_CLOSE_SEC = 2.5;
